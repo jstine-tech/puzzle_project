276378fa8df6a29141585fdf591f03bb7c9f5b63
Jonathan Stine <jonathanstine96@gmail.com>
2019-03-27 18:42:29 -0400

    Started working on the js, made the randomize button

--- puzzle.js
+++ puzzle.js
@@ -1,4 +1,26 @@
 var Display = function(puzzle) { //expects a string for the id of the puzzle div
     this.puzzleID = puzzle;
     this.tableID = jQuery('#puzzle > table');
-}
+    this.numberOfColumns = jQuery('#row1').children().length;
+    this.numberOfRows = jQuery('tr').length;
+    this.matrixOfPuzzle = new Array(this.numberOfRows);
+    this.refreshMatrix = function() {
+        let pieces = jQuery('td');
+        let currentColumn = 0;
+        let currentRow = 0;
+        pieces.each(function(index){
+            if(!(currentColumn < this.numberOfColumns)) {
+                currentColumn = 0;
+                currentRow++;
+            }
+            this.matrixOfPuzzle[currentRow][currentColumn] = parseInt(this.html());
+            currentColumn++;
+        });
+    }
+};
+
+var theGame = new Display('puzzle')
+
+jQuery('td').click(function() {
+
+});
